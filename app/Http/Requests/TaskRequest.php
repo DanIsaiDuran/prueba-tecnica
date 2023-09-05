7b11b7e534076d4ef3d61f537c2f37f8f7072c07
DanIsaiDuran <dan.duran5635@alumnos.udg.mx>
2023-09-05 16:26:40 -0600

    Se creo un custom request para validar la creacion de tasks

---
+++ app/Http/Requests/TaskRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class TaskRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'description' => 'required|min:5|max:255',
+            'user_id' => 'required',
+            'company_id' => 'required',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
